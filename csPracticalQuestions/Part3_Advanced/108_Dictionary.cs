54112188ad68865553f256e7defdae033da312eb
James <woghd8503@naver.com>
2020-01-20 11:00:00 +0900

    Part3_Advanced

--- csPracticalQuestions/Part3_Advanced/108_Dictionary.cs
+++ csPracticalQuestions/Part3_Advanced/108_Dictionary.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Hashtable나 Dictionary<TKey, TValue> 객체는 사전과 같이 "키"와 "값" 쌍으로 데이터를 저장합니다. 0, 1, 2, ...와 같은 인덱스가 아니고 키에 의해 값을
+// 찾아줍니다. Hashtable이나 Dictionary에서는 모든 값이 키가 될 수 있습니다. Hashtable은 non-generic 컬렉션이고 Dictionary<TKey, TValue>는 제레릭 컬렉션입니다.
+
 namespace ConsoleApp1
 {
     class Program
