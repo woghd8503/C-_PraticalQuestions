dd4b31bd0f47f76367ff9f65df5dfb83e8ecaa96
James <woghd8503@naver.com>
2021-11-02 11:28:09 +0900

    Part1_Basic

---
+++ csPracticalQuestions/ConsoleApp1/001_ReadAndWrite.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello ");
+            Console.WriteLine("World!");
+            Console.WriteLine("이름을 입력하세요: ");
+
+            string name = Console.ReadLine();        // 이름을 입력받습니다.
+            Console.WriteLine("안녕하세요, ");
+            Console.WriteLine(name);
+            Console.WriteLine("님!");
+        }
+    }
+}
