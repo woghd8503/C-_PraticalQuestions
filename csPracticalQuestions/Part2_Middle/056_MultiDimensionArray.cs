15adc5e5d277b850caa8c147d65cdf507967b921
James <woghd8503@naver.com>
2021-11-20 23:49:10 +0900

    Part2_Middle

--- csPracticalQuestions/Part2_Middle/056_MultiDimensionArray.cs
+++ csPracticalQuestions/Part2_Middle/056_MultiDimensionArray.cs
@@ -17,15 +17,22 @@ namespace ConsoleApp1
             for(int i = 0; i < 2; i++)
             {
                 for ( int j = 0; j < 3; j++)
-                    Console.WriteLine(");
+                    Console.Write("{0,5}", arrA[i, j]);
+                Console.WriteLine();
             }
-        }
 
-        private static void PrintArray(int[] a)
-        {
-            foreach(var i in a)
-                Console.Write("{0,5}", i);
-            Console.WriteLine();
+            Console.WriteLine("가변 배열: arrB[2][3]");
+            int[][] arrB = new int[2][];
+            arrB[0] = new int[] { 1, 2 };
+            arrB[1] = new int[] { 3, 4, 5 };
+
+            for (int i = 0; i < 2; i++)
+            {
+                Console.Write("arrB[{0}] : ", i);
+                for(int j = 0; j < arrB[i].Length; j++)
+                    Console.Write("{0} ", arrB[i][j]);
+                Console.WriteLine();
+            }
         }
     }
 }
