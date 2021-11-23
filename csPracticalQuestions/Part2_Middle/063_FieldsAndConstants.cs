e6ea954fd5b955fb1f16d6910c574a74e390c1a1
James <woghd8503@naver.com>
2021-11-24 00:04:57 +0900

    Part2_Middle

--- csPracticalQuestions/Part2_Middle/063_FieldsAndConstants.cs
+++ csPracticalQuestions/Part2_Middle/063_FieldsAndConstants.cs
@@ -20,8 +20,26 @@ namespace ConsoleApp1
 
     class MyCalendar
     {
-        public const int month = 12;
+        public const int months = 12;
+        public const int weeks = 52;
+        public const int days = 365;
 
+        public const double daysPerWeek = (double)days / (double)weeks;
+        public const double daysPerMonth = (double)days / (double)months;
+    }
+
+    class MemberVariables
+    {
+        static void Main(string[] args)
+        {
+            Product p = new Product();
+            p.name = "시계";
+            p.price = 100000;
+
+            Console.WriteLine("{0} : {1:C}", p.name, p.price);
+            Console.WriteLine("원주율: {0}", MyMath.PI);
+            Console.WriteLine("한 달은 평균 {0:F3}일", MyCalendar.daysPerMonth);
+        }
     }
 }
 
