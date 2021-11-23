1a2d9b6ca9816b198bd84f576777faf312616dba
James <woghd8503@naver.com>
2021-11-23 23:57:26 +0900

    Part2_Middle

---
+++ csPracticalQuestions/Part2_Middle/063_FieldsAndConstants.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Product
+    {
+        public string name;
+        public int price;
+    }
+
+    class MyMath
+    {
+        public static double PI = 3.14;
+    }
+
+    class MyCalendar
+    {
+        public const int month = 12;
+
+    }
+}
+
+
