5dde97dd493f9f6754d0d2f505f4c94d70215431
James <woghd8503@naver.com>
2021-11-28 10:42:20 +0900

    Part2_Middle

--- csPracticalQuestions/Part2_Middle/068_IsPrime.cs
+++ csPracticalQuestions/Part2_Middle/068_IsPrime.cs
@@ -13,19 +13,18 @@ namespace ConsoleApp1
         {
             // 1~100까지의 소수를 찾는 프로그램
             int count = 0;
-
             for (int i = 2; i <= 100; i++)
                 if (IsPrime(i))
                 {
                     Console.Write("{0} ", i);
                     count++;
                 }
-            Console.Write("\n2~100까지 소수는 모두{0}개 있습니다.", count);
+            Console.WriteLine("\n2~100까지 소수는 모두 {0}개 있습니다.", count);
         }
 
         private static bool IsPrime(int x)
         {
-            for(int i = 2; i < x; i++)
+            for (int i = 2; i < x; i++)
             {
                 if (x % i == 0)
                     return false;
