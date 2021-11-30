87db4f41361e14678f07820a8f08eee4f638cfb3
James <woghd8503@naver.com>
2021-12-01 00:18:48 +0900

    Part2_Middle

---
+++ csPracticalQuestions/Part2_Middle/084_TimeSpan.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Methods
+    {
+        static void Main(String[] args)
+        {
+            DateTime christmas = new DateTime(2018, 12, 25);
+            DateTime newYearsDay = new DateTime(2019, 1, 1);
+
+            TimeSpan span = newYearsDay - christmas;
+            // TimeSpan span = newYearsDay.Subtract(christmas);
+            Console.WriteLine("크리스마스와 1월 1일의 시간 간격");
+            Console.WriteLine("{0,14}", span);
+            Console.WriteLine("{0,14} days", span.Days);
+            Console.WriteLine("{0,14} hours", span.Hours);
+            Console.WriteLine("{0,14} minutes", span.Minutes);
+            Console.WriteLine("{0,14} seconds", span.Seconds);
+            Console.WriteLine("{0,14} milliseconds", span.Milliseconds);
+
+            Console.WriteLine("또는");
+            Console.WriteLine("{0,14}", span);
+            Console.WriteLine("{0,14} days", span.TotalDays);
+            Console.WriteLine("{0,14} hours", span.TotalHours);
+            Console.WriteLine("{0,14} minutes", span.TotalMinutes);
+            Console.WriteLine("{0,14} seconds", span.TotalSeconds);
+            Console.WriteLine("{0,14} milliseconds", span.TotalMilliseconds);
+            Console.WriteLine("{0,14} ticks", span.Ticks);
+        }
+    }
+}
+
+
