d7710d00aceddf5dcd9b75066ef667a010e7a41f
James <woghd8503@naver.com>
2021-12-02 07:57:44 +0900

    Part2_Middle

---
+++ csPracticalQuestions/Part2_Middle/085_LifeTimeCalc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Methods
+    {
+        static void Main(String[] args)
+        {
+            Console.Write("생년월일 시분초를 입력하세요: ");
+            DateTime date1 = DateTime.Parse(Console.ReadLine());
+            DateTime date2 = DateTime.Now;
+
+            TimeSpan interval = date2 - date1;
+            Console.WriteLine("탄생 시간: {0}", date1);
+            Console.WriteLine("현재 시간: {0}", date2);
+            Console.WriteLine("생존 시간: {0}", interval.ToString());
+            Console.WriteLine("당신은 지금 이 순간까지 {0}일 {1}시간" + " {2}분 {3}초를 살았습니다.",
+            interval.Days, interval.Hours, interval.Days, interval.Hours, interval.Minutes, interval.Seconds);
+        }
+    }
+}
+
+
