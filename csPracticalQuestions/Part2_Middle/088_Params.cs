f26c759eb68d115cbb05d49e6c6251e2448fe971
James <woghd8503@naver.com>
2021-12-05 23:49:17 +0900

    Part2_Middle

---
+++ csPracticalQuestions/Part2_Middle/088_Params.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Methods
+    {
+        public static void PrintIntParams(params int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public static void PrintObjectParams(params object[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(String[] args)
+        {
+            PrintIntParams(1, 2, 3, 4);
+            PrintObjectParams(1, 1.234, 'a', "test");
+            PrintObjectParams();
+
+            int[] myIntArray = { 5, 6, 7, 8, 9 };
+            PrintIntParams(myIntArray);
+
+            object[] myObjArray = { 2, 2.2345, 'b', "test", "again" };
+            PrintObjectParams(myObjArray);
+
+            PrintObjectParams(myIntArray);
+        }
+    }
+}
