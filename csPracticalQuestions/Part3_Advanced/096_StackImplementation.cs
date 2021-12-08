347ff824f8460e085b0769501786950eeeec5a6e
James <woghd8503@naver.com>
2021-12-09 08:02:28 +0900

    Part2_Advanced

---
+++ csPracticalQuestions/Part3_Advanced/096_StackImplementation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class MyStack<T>
+    {
+        const int maxSize = 10;
+        private T[] arr = new T[maxSize];
+        private int top;
+
+        public MyStack()
+        {
+            top = 0;
+        }
+
+        public void Push(T val)
+        {
+            if (top < maxSize)
+            {
+
+            }
+        }
+      static void Main(string[] args)
+        {
+            
+        }
+    }
+}
