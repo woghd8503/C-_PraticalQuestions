816caef23f5293a5204ecb04f11a12d885c455b3
James <woghd8503@naver.com>
2021-12-10 01:53:36 +0900

    Part3_Advanced

---
+++ csPracticalQuestions/Part3_Advanced/097_StackImplementation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack<int> stack = new MyStack<int>();
+            Random r = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                int val = r.Next(100);
+                stack.Push(val);
+                Console.Write("Push");
+            }
+        }
+            
+    }
+
+}
