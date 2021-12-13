e68f3b9bf20405e360caef4da6ae6e01c2903d67
James <woghd8503@naver.com>
2021-12-14 00:37:08 +0900

    Part3_Advanced

--- csPracticalQuestions/Part3_Advanced/101_List.cs
+++ csPracticalQuestions/Part3_Advanced/101_List.cs
@@ -12,11 +12,10 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            ArrayList a = new ArrayList();
+            List<int> a = new List<int>();
             Random r = new Random();
 
             PrintValues(a);
-
             for (int i = 0; i < 10; i++)
                 a.Add(r.Next(100));
 
@@ -28,13 +27,13 @@ namespace ConsoleApp1
             PrintValues(a);
         }
 
-        private static void PrintValues(ArrayList a)
+        private static void PrintValues(List<int> a)
         {
-            Console.WriteLine("Print in ArrayList");
-            Console.WriteLine("  Count = {0}", a.Count);
-            Console.WriteLine("  Capacity = {0}", a.Capacity);
+            Console.WriteLine("Print Values in List<int>");
+            Console.WriteLine("   Count = {0}", a.Count);
+            Console.WriteLine("   Capacity = {0}", a.Capacity);
             foreach (var i in a)
-                Console.Write("  {0}", i);
+                Console.Write("   {0}", i);
             Console.WriteLine();
         }
     }
---
+++ csPracticalQuestions/Part3_Advanced/102_AlphabeticalSort.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> IstNames = new List<string>();
+            IstNames.Add("dog");
+            IstNames.Add("cow");
+            IstNames.Add("rabbit");
+            IstNames.Add("goat");
+            IstNames.Add("sheep");
+            IstNames.Sort();
+            foreach (string s in IstNames)
+                Console.Write(s + " ");
+            Console.WriteLine();
+
+            string[] arrNames = new string[100];
+            arrNames[0] = "dog";
+            arrNames[1] = "cow";
+            arrNames[2] = "rabbit";
+            arrNames[3] = "goat";
+            arrNames[4] = "sheep";
+            Array.Sort(arrNames);
+            foreach (string s in IstNames)
+                Console.Write(s + " ");
+            Console.WriteLine();
+        }
+    }
+}
