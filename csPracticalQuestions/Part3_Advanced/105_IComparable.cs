6768cc70993b5908c436c7c2d4fd914da1e3d4a0
James <woghd8503@naver.com>
2021-12-16 06:13:51 +0900

    Part3_Advanced

---
+++ csPracticalQuestions/Part3_Advanced/105_IComparable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    static void Main(string[] args)
+    {
+        Artists[] famousArtists =
+        new Artists("레오나르도 다빈치", "이탈리아", 1452, 1519),
+        new Artists("빈센트 반 고흐", "네덜란드", 1852, 1890),
+        new Artists("클로드 모네", "프랑스", 1840, 1926),
+        new Artists("파블로 피카소", "스페인", 1881, 1973),
+        new Artists("베르메르", "네덜란드", 1632, 1675),
+        new Artists("르노아르", "프랑스", 1841, 1919)
+};
+
+    List
+}
