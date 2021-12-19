246aa1b792f8a424f6b00d8f923307cd7ec1c607
James <woghd8503@naver.com>
2021-12-20 01:09:00 +0900

    Part3_Advanced

---
+++ csPracticalQuestions/Part3_Advanced/109_SortedList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList<int, string> s1 = new SortedList<int, string>();
+            s1.Add(3, "Tree");
+            s1.Add(4, "Four");
+            s1.Add(1, "One");
+            s1.Add(2, "Two");
+
+            for (int i = 0; i < s1.Count; i++)
+                Console.WriteLine("k: {0}, v: {1} / ", s1.Keys[i], s1.Values[i]);
+            Console.WriteLine();
+
+            foreach(var kvp in s1)
+                Console.Write("{0, -10} ", kvp);
+            Console.WriteLine();
+
+            SortedList<String, int> s2 = new SortedList<string, int>();
+            s2.Add("one", 1);
+        }
+    }
+}
+
