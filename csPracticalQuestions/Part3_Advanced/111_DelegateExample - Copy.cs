4c7d5f453393f2d18b2a6d34f1accb50f5f7dee6
James <woghd8503@naver.com>
2021-12-23 08:55:12 +0900

    Part3_Advanced

---
+++ csPracticalQuestions/Part3_Advanced/111_DelegateExample - Copy.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        delegate bool MemberTest(int a);
+
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 3, 5, 4, 2, 6, 4, 6, 8, 54, 23, 4, 6, 4 };
+
+            Console.WriteLine("짝수의 개수: " + Count(arr, IsEven));
+            Console.WriteLine("홀수의 개수: " + Count(arr, IsOdd));
+        }
+
+        static int Count(int[] a, MemberTest testMethod)
+        {
+            int cnt = 0;
+            foreach (var n in a)
+            {
+                if (testMethod(n) == true)
+                    cnt++;
+            }
+            return cnt;
+        }
+
+        static public bool IsOdd(int n) { return n % 2 != 0; }
+        static public bool IsEven(int n) { return n % 2 == 0; }
+    }
+}
+
---
+++ csPracticalQuestions/Part3_Advanced/112_AnonymousDelegate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        delegate bool MemberTest(int x);
+
+        static void Main(string[] args)
+        {
+            var arr = new[] { 3, 34, 6, 34, 7, 8, 24, 3, 675, 8, 23 };
+
+            int n = Count(arr, delegate (int x) { return x % 2 == 0; });
+            Console.WriteLine("짝수의 개수: " + n);
+
+            n = Count(arr, delegate (int x) { return x % 2 != 0; });
+            Console.WriteLine("홀수의 개수: " + n);
+        }
+
+        private static int Count(int[] arr, MemberTest testMethod)
+        {
+            int cnt = 0;
+            foreach (var n in arr)
+            {
+                if (testMethod(n))
+                    cnt++;
+            }
+            return cnt;
+        }
+    }
+}
+
