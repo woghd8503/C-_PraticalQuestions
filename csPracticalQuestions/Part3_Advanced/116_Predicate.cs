517ef0e385fb943d30687c59840f923100fe9221
James <woghd8503@naver.com>
2021-12-25 15:15:33 +0900

    Part3_Advanced

---
+++ csPracticalQuestions/Part3_Advanced/116_Predicate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<int> isEven = n => n % 2 == 0;
+            Console.WriteLine(isEven(6));
+
+            Predicate<string> isLowerCase = s => s.Equals(s.ToLower());
+            Console.WriteLine(isLowerCase("This is test"));
+        }
+    }
+}
+
---
+++ csPracticalQuestions/Part3_Advanced/117_ListAndLambda.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> myList = new List<String>
+            {
+                "mouse", "cow", "tiger", "rabbit", "dragon", "snake"
+            };
+
+            bool n = myList.Exists(s => s.Contains("x"));
+            Console.WriteLine("이름에 'x'를 포함하는 동물이 있나요: " + n);
+
+            Console.Write("이름이 3글자인 첫 번째 동물들: ");
+            string name = myList.Find(s => s.Length == 3);
+            Console.WriteLine(name);
+
+            Console.Write("이름이 6글자 이상의 동물들: ");
+            List<string> longName = myList.FindAll(s => s.Length > 5);
+            foreach (var item in longName)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            Console.Write("대문자로 변환: ");
+            List<string> capList = myList.ConvertAll(s => s.ToUpper());
+            foreach(var item in capList)
+                Console.Write(item + " ");
+            Console.WriteLine();
+        }
+    }
+}
+
