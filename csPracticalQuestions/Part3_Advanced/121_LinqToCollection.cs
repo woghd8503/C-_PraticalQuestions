d7361664d531e6b67dc3cc494a2ea4c5e72d1de0
James <woghd8503@naver.com>
2021-12-28 23:52:21 +0900

    Part3_Advanced

---
+++ csPracticalQuestions/Part3_Advanced/121_LinqToCollection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public List<int> Scores { get; set; }
+    }
+
+    class Program
+    {
+        static List<Student> students;
+
+        static void Main(string[] args)
+        {
+            students = new List<Student>
+            {
+                new Student { Name="Pjkim", Id=19001001, Scores = new List<int>{86,90,76}},
+                new Student { Name="BsKim", Id=}
+            }
+        }
+    }
+}
+
