4e56f6899c157ab10eaee16f57e6e4750321f800
James <woghd8503@naver.com>
2021-12-31 16:09:35 +0900

    Part3_Advanced

--- csPracticalQuestions/Part3_Advanced/121_LinqToCollection.cs
+++ csPracticalQuestions/Part3_Advanced/121_LinqToCollection.cs
@@ -24,8 +24,36 @@ namespace ConsoleApp1
         {
             students = new List<Student>
             {
-                new Student { Name="Pjkim", Id=19001001, Scores = new List<int>{86,90,76}},
-                new Student { Name="BsKim", Id=}
+                new Student { Name="PjKim", Id=19001001, Scores = new List<int>{86,90,76}},
+                new Student { Name="BsKim", Id=19001002, Scores = new List<int>{56,92,93}},
+                new Student { Name="YsCho", Id=19001003, Scores = new List<int>{69,85,75}},
+                new Student { Name="BiKang", Id=19901004, Scores = new List<int>{88,80,57}}
+            };
+
+            Print(students);
+            HighScore(0, 85); // 첫 번째 시험에서 85점 이상인 학생 출력
+            HighScore(1, 90); // 두 번째 시험에서 90점 이상인 학생 출력
+        }
+
+        private static void HighScore(int exam, int cut)
+        {
+            var highScore = from student in students
+                            where student.Scores[exam] >= cut
+                            select new { Name = student.Name, Score = student.Scores[exam] };
+
+            Console.WriteLine($"{exam + 1}번째 시험에서 {cut} 이상의 점수를 받은 학생");
+            foreach (var item in highScore)
+                Console.WriteLine($"\t{item.Name, -10}{item.Score}");
+        }
+
+        private static void Print(List<Student> students)
+        {
+            foreach (var item in students)
+            {
+                Console.Write($"{item.Id,-10}{item.Name,-10}");
+                foreach (var score in item.Scores)
+                    Console.Write($"{score,-5}");
+                Console.WriteLine(item.Scores.Average().ToString("F2"));
             }
         }
     }
