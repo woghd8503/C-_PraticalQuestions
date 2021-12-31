89e00e2c0a81fd658af0f773d11882c566578917
djy1020 <53558537+djy1020@users.noreply.github.com>
2021-12-31 13:22:52 +0900

    Part3_Advanced

--- csPracticalQuestions/Part3_Advanced/121_LinqToCollection.cs
+++ csPracticalQuestions/Part3_Advanced/121_LinqToCollection.cs
@@ -25,8 +25,10 @@ namespace ConsoleApp1
             students = new List<Student>
             {
                 new Student { Name="Pjkim", Id=19001001, Scores = new List<int>{86,90,76}},
-                new Student { Name="BsKim", Id=}
-            }
+                new Student { Name="BsKim", Id=19001002, Scores = new List<int>{56,92,93}},
+                new Student { Name="YsCho", Id=19001003, Scores = new List<int>{69,85,75}},
+                new Student { Name="Bikang",Id=19001004, Scores = new List<int>{88,80,57}}
+            };
         }
     }
 }
