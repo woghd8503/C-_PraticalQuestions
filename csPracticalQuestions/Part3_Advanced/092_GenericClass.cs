49f8431feb91d9144802e9ba5f9c7b51534aade3
James <woghd8503@naver.com>
2022-01-02 03:29:03 +0900

    Part3_Advanced

--- csPracticalQuestions/Part3_Advanced/092_GenericClass.cs
+++ csPracticalQuestions/Part3_Advanced/092_GenericClass.cs
@@ -4,6 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// 일반화 (제너릭)클래스는 멤버의 형을 <T>로 표시한 클래스이다. 객체 생성 시에 그 멤버의 형이 결정된다.
+// 일반화 클래스 하나를 정의 
+// class MyClass<T>
+// {
+//    public T x;
+//    public T DoSomething(T p) { ... }
+// }
+
+// 클래스 이름 뒤의 <T>는 형식 매개변수라고 한다. 객체가 생성될 때 < > 안에 쓰는 형이 T를 대체하게 된다.
+// 다음의 코드에서 x는 <int>를 사용했으므로 클래스의 정의에서 T로 쓴 부분이 int로 바귀게 되어 다음과 같은 클래스가
+// 정의된 것과 같이 동작합니다. <string>을 쓴 두 번째 객체 s는 T가 string으로 바뀐 클래스를 사용하게 된다.
+// MyClass<int> x = new MyClass<int>();
+// MyClass<string> s = new MyClass<string>();
+// 일반화 클래스를 사용하면 같은 구조와 동작을 갖지만 형만 다른 클래스를 중복해서 작성하지 않아도 된다.
+
 namespace Part3_Advanced
 {
     class MyClass<T>
