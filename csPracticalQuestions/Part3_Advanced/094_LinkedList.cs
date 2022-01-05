19d243bde78a56b970a8212440391dbfb4afc6cc
James <woghd8503@naver.com>
2022-01-05 23:39:18 +0900

    Part3_Advanced

--- csPracticalQuestions/Part3_Advanced/094_LinkedList.cs
+++ csPracticalQuestions/Part3_Advanced/094_LinkedList.cs
@@ -8,6 +8,27 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
+    // List<T>나 LinkedList<T> 클래스는 C#에서 컬렉션으로 제공하고 있다.
+    // 하지만 이러한 자료구조를 직접 만드는 것이 프로그램 공부에 매우 도움이 된다.
+    // 이번에는 연결리스트 LinkedList를 직접 구현해 보겠습니다. LinkedList 클래스를
+    // 별도의 파일에 만들겠다. 이렇게 하는 이유는 LinkedList를 만들어 두고 다른 프로그램에서도
+    // 쓸 수 있기 때문입니다. 보통 클래스는 클래스 이름과 같은 cs 파일에 만듭니다.
+    
+    // 클래스 파일에 별도로 만들 때는 솔루션 탐색기의 프로젝트 이름 위에서 마우스 오른쪽 버튼을
+    // 누르고 나타나는 팝업 메뉴에서 "추가"-"클래스"를 선택하고 클래스 이름을 입력하면 된다.
+    // 여기서는 클래스 이름과 같은 LinkedList.cs라는 파일을 추가한다.
+    // LinkedList.cs 파일에는 Node 클래스와 LinkedList 클래스의 두개의 클래스를 만듭니다.
+    // Node 클래스는 리스트의 각 노드를 표현하는 클래스이며 아래 표와 같이 Node 타입의
+    // head 필드와 7개의 메소드로 구성된다. 연결리스트에 대한 자세한 설명은 생략
+
+    // [Node 클래스의 필드와 메소드]
+    //  int date : 노드에 자장되는 값입니다.
+    //  Node next : 리스트에서 다음에 연결되는 노드입니다.
+    //  Node(int data) : Node 클래스의 생성자입니다. data를 저장하는 Node를 생성한다.
+
+    // [LinkedList 클래스의 필드와 메소드]
+    // Node head : 리스트의 첫 번째 노드입니다.
+    // void InsertFront(int data) : 리스트의 맨 앞에 data를 추가합니다.
     class Node
     {
         internal int data;
--- csPracticalQuestions/Part3_Advanced/098_QueueImplementation.cs
+++ csPracticalQuestions/Part3_Advanced/098_QueueImplementation.cs
@@ -6,6 +6,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// 선입선출(Fast-in-first-out, FIFO) 형태의 자료를 다룰 때는 큐를 사용합니다. 선착순으로 제일 먼저 들어온 자료가
+// 제일 먼저 들어온 자료가 제일 먼저 나가는 자료구조입니다. C#의 컬렌션에는 Queue <T>가 있어서 큐가 필요하면 .NET에서 제공되는 Queue<T>를 쓰면 됩니다.
+// 하지만 이번 장에서는 직접 큐를 만들겠습니다. 컬렉션에서 제공하는 Queue <T>와 구분하기 위해서 MyQueue <T>를 만듭니다. 프로젝트는 Main()이 있는 Program
+// 클래스가 있는 program.cs와 MyQueue <T>클래스를 정의하는 myQueue.cs 파일 두 개로 구성합니다.
+
+// Queue.cs  파일에서 MyQueue <T> 클래스를 정의합니다. 큐에 저장되는 값이 여러 가지 타입일 수 있으므로 제너릭으로 정의합니다. MyQueue
 namespace ConsoleApp1
 {
     class Node<T>
