6b0069ddff48dca1f2bdac63afbac3973289e871
James <woghd8503@naver.com>
2022-01-13 23:50:51 +0900

    Part3_Advanced

--- csPracticalQuestions/Part3_Advanced/106_Queue.cs
+++ csPracticalQuestions/Part3_Advanced/106_Queue.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// 극장이나 음식점에서 순서를 기다리면서 줄을 서면 먼저 온 사람이 먼저 들어갑니다. 이와 같이 큐 (Queue)는 자료가
+// 먼저 처리되므로 선입선출(FIFO, First-In First-Out)이라고도 합니다.
 namespace ConsoleApp1
 {
     class Program
