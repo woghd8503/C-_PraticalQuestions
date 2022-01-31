413ec19c07643532f21f4e3ff6549e1c731d3b43
James <woghd8503@naver.com>
2022-01-31 13:54:38 +0900

    Part3_Advanced

--- csPracticalQuestions/Part3_Advanced/114_LambdaExpression.cs
+++ csPracticalQuestions/Part3_Advanced/114_LambdaExpression.cs
@@ -6,7 +6,11 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+// 람다식은 익명 메소드를 간단하게 표현할 수 있는 방법입니다. 람다식은 개체로 처리되는 코
+// 드 블록( 식 또는 문 블록 )이며 메소드와 같이 매개변수와 리턴 값을 갖습니다. 람다식은 인수를
+// 메소드에 전달할 수 있으며 값을 반환할 수 있습니다.
 
+// 람다식은 델리게이    
 namespace ConsoleApp1
 {
     class Program
