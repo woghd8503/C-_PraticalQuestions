8e8f06d4a2785bd64e11cd06c930cff02c63ecf5
JamesPark <woghd8503@naver.com>
2022-02-07 02:28:03 +0900

    Part3_Advanced

--- csPracticalQuestions/Part3_Advanced/117_ListAndLambda.cs
+++ csPracticalQuestions/Part3_Advanced/117_ListAndLambda.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Predicate <T> 델리게이트는 리턴 값이 반드시 bool이고 입력 파라미터가 하나인 델리게이트입니다. Predicate<T>는 Array나 List 클래스의
+// 메소드들에서 자주 사용됩니다. 예를 들어 List<T> 클래스에서 Predicate<T>를 사용하는 메소드들은 다음과 같습니다.
+
 namespace ConsoleApp1
 {
     class Program
