c05ee0216d30dc2ea064e7b7186e1d6ada54c20b
JamesPark <woghd8503@naver.com>
2022-02-09 03:06:10 +0900

    Part3_Advanced

--- csPracticalQuestions/Part3_Advanced/118_LinqBasic1.cs
+++ csPracticalQuestions/Part3_Advanced/118_LinqBasic1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 
 // LINQ는 Language-INtegrated Query의 약자로 C#에 통합된 데이터 질의 기능입니다. SQL과 같은 Query 언어는 데이터베이스에서 사용되던 언어인데,
+// 이를 C#에 도입한 것입니다. LINQ를 사용하면 컬렉션에서 데이터를 다루는 방법이 훨신 간단해집니다. LINQ는 람다와 함께 C# 3.0에 도입된 기능입니다.
+
 //
 namespace ConsoleApp1
 {
