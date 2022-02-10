1715572e8ce9767b5d838f0f0be584c730f5f1b9
James <woghd8503@naver.com>
2022-02-11 00:22:30 +0900

    Part3_Advnaced

--- csPracticalQuestions/Part3_Advanced/122_LinqGroupBy.cs
+++ csPracticalQuestions/Part3_Advanced/122_LinqGroupBy.cs
@@ -7,7 +7,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-// LINQ에서 Group by는 특정 조건으로 데이터를 분류하는 기능입니다. 예를 들어 학생들의 평균 점수가 80점 이상인 그룹과 80점 미만인
+// LINQ에서 Group by는 특정 조건으로 데이터를 분류하는 기능입니다. 예를 들어 학생들의 평균 점수가 80점 이상인 그룹과 80점 미만인 그룹으로
+// 각각의 평균, 최대, 최소값 등을 구할 수 있습니다. Group by 문은 다음과 같이 사용됩니다.
+
+// group a by into g
+
+// a는 from 문에서 사용한 변수이고, b는 분류 기준이며, c는 그룹 변수입니다. 즉, a에서
 
 namespace ConsoleApp1
 {
