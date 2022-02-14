cd04772fb81d9c307631693bc1f17e03abead0f5
James <woghd8503@naver.com>
2022-02-14 23:58:33 +0900

    Part2_Middle

--- csPracticalQuestions/Part2_Middle/054_ArrayBasic.cs
+++ csPracticalQuestions/Part2_Middle/054_ArrayBasic.cs
@@ -7,6 +7,10 @@ using System.Threading.Tasks;
 
 // 배열은 같은 자료형을 같는 자료들의 연속된 집합입니다 C#에서 배열은 참조형이므로 다음과 같이 new 키워드를 사용
 // 하여 생성합니다.
+
+// 자료형[] 배열명 = new 자료형[N];  // N은 배열의 크기
+
+// 배열은 선언과 동시에 초기화할 수 있습니다. 중괄호
 namespace ConsoleApp1
 {
     class Program
